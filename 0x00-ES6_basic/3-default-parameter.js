542f21561e14c538cf211debcaa2f94c984e276c
Boniface <113481026+onoyima@users.noreply.github.com>
2024-07-24 14:01:37 +0100

    Update 3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,6 +1,3 @@
-// File: 3-default-parameter.js
-
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
-
