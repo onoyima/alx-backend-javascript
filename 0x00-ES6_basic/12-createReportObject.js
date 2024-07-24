e9b044af43c689e9b35023a53d28ed431c1092fb
Boniface <113481026+onoyima@users.noreply.github.com>
2024-07-24 13:57:58 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,8 @@
-// File: 12-createReportObject.js
-
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    }
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
   };
 }
-
