381214a85d3c51932594133b67fcc165c4697999
Boniface <113481026+onoyima@users.noreply.github.com>
2024-07-25 19:15:52 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new TypeError('Cannot construct Building instances directly');
-    }
-    if (typeof sqft !== 'number') throw new TypeError('Square feet must be a number');
-
     this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
