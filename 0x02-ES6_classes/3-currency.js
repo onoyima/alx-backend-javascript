3beb65a4de1835ee183a4fa761c92bf89f7d5df6
Boniface <113481026+onoyima@users.noreply.github.com>
2024-07-25 19:14:30 +0100

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,8 +1,5 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') throw new TypeError('Code must be a string');
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-
     this._code = code;
     this._name = name;
   }
@@ -11,18 +8,16 @@ export default class Currency {
     return this._code;
   }
 
-  set code(value) {
-    if (typeof value !== 'string') throw new TypeError('Code must be a string');
-    this._code = value;
+  set code(n) {
+    this._code = n;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') throw new TypeError('Name must be a string');
-    this._name = value;
+  set name(n) {
+    this._name = n;
   }
 
   displayFullCurrency() {
