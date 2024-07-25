c8e9ee432cdb68c8d1005bb01a4950cee99874ea
Boniface <113481026+onoyima@users.noreply.github.com>
2024-07-25 19:15:32 +0100

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,10 +1,5 @@
-import Currency from './3-currency.js';
-
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
-    if (!(currency instanceof Currency)) throw new TypeError('Currency must be a Currency object');
-
     this._amount = amount;
     this._currency = currency;
   }
@@ -13,28 +8,23 @@ export default class Pricing {
     return this._amount;
   }
 
-  set amount(value) {
-    if (typeof value !== 'number') throw new TypeError('Amount must be a number');
-    this._amount = value;
+  set amount(n) {
+    this._amount = n;
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    if (!(value instanceof Currency)) throw new TypeError('Currency must be a Currency object');
-    this._currency = value;
+  set currency(n) {
+    this._currency = n;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Amount and conversion rate must be numbers');
-    }
     return amount * conversionRate;
   }
 }
