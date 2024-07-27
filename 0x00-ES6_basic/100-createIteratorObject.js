f3d27c3230acb3877d71866d4e129fcd21ca74ad
Boniface <113481026+onoyima@users.noreply.github.com>
2024-07-27 15:25:48 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,13 @@
 export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (let department of Object.values(report.allEmployees)) {
+    allEmployees = [...allEmployees, ...department];
   let result = [];
   for (const value of Object.values(report.allEmployees)) {
     result = [...result, ...value];
   }
+  return allEmployees;
 
   return result;
 }
+  
