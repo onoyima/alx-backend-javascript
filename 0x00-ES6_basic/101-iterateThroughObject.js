76f093e68dfcd50717391c3283dee91aafcea3b0
Boniface <113481026+onoyima@users.noreply.github.com>
2024-07-27 15:05:57 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,4 @@
-export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+export default function createIteratorObject(report) {
+  const employees = Object.values(report.allEmployees).flat();
+  return employees[Symbol.iterator]();
 }
